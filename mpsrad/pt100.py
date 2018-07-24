8f3980cb802f884a7cda54ea5048eeb04925437b
Richard Larsson <ric.larsson@gmail.com>
2018-07-24 15:00:45 +0200

    Documentation examples and adding math to conf.py

--- mpsrad/pt100.py
+++ mpsrad/pt100.py
@@ -5,115 +5,189 @@ Last modification: 29.03.2018
 
 Author: Borys Dabrowski
 
-Code to measure resistance and convert it in temperature. 
+Code to measure resistance and convert it in temperature.
 """
 
 import socket,time
 
 class pt100:
-	""" Interactions with wiltron68169B functionality
+    """ Interactions with PT-100 functionality
+    """
 
-	Loads the shared wiltron-library as ../lib/libwiltron68169B.so on import
-	and handles translation of python input to ctypes and ctypes output to
-	python-readable variables
+    def __init__(self, address=22,host='192.168.5.10', tcp_port=1234,
+            timeout=5, name="pt100-python"):
+        """PT-100 interface
 
-	Functions:
-		ask_name:
-			Asks the name (to see it there is a response)
+        Communication and temperature readouts from a PT-100
 
-		set_frequency:
-			Sets the frequency to provided GHz
-	"""
+        Parameters:
+            address (int):
+                Address of the machine
+            host (str):
+                Name of the host, IP or DNS
+            tcp_port (int):
+                Port to communicate with the PT-100 over
+            timeout (float, int):
+                Time allowed to wait for data before moving on, if no data is
+                received during this time, nothing special happens
+            name (any):
+                Name of this machine (unused, kept as housekeeping)
 
-	def __init__(self, address=22,host='192.168.5.10', tcp_port=1234,
-			timeout=5, name="pt100-python"):
-		self._initialized=False
-		self._address=address
+        Example:
+            >>> s = pt100()
+            >>> s.init()
+            >>> print(round(s.get_temperature(), 2), 'K')
+            295.23 K
+            >>> s.close()
 
-		# Host information
-		self._tcp_port=tcp_port
-		self._host=host
-		self._timeout=timeout
+        """
+        self._initialized = False
+        self._address = address
 
-	def ohm2celsius(self,R_PT100):
-		"""Converts Pt100 Ohm to deg. Celsius"""
-		R0=100.0
-		c=[-1.138628e-03, 2.559038e+00, 9.988525e-04, -1.061679e-06, 2.585121e-08]
+        # Host information
+        self._tcp_port = tcp_port
+        self._host = host
+        self._timeout = timeout
 
-		temp=c[4]
-		for n in [3, 2, 1, 0]: temp=temp*(R_PT100-R0)+c[n]
-		return temp
+        # Housekeeping
+        self.name = name
 
-	def init(self):
-		assert not self._initialized, "Cannot init initialized machine"
+    def ohm2celsius(self, R_PT100):
+        """Converts PT-100 Ohm to degrees Celsius
 
-		# Open socket
-		s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		s.connect((self._host,self._tcp_port))
-		s.setblocking(0)	# make socket non blocking
+        Solves the following expression
 
-		# Set address
-		s.sendall(b'++addr '+str(self._address).encode('ascii')+'\r\n')
+        .. math::
+            T = c_0 - (100 - R_{PT100})(c_1 - (100 - R_{PT100})(c_2 - \
+            (100 - R_{PT100})(c_3 - c_4(100 - R_{PT100}))))
 
-		# Send setup
-		s.sendall(b'++clr\r\n')
-		time.sleep(2)
-		s.sendall(b'F3 R2 N5 Z1 T4 M35 \r\n')
-		time.sleep(2)
-		s.sendall(b'F3 R2 N5 Z1 T4 M35 \r\n')
+        where the :math:`c_n`-coefficients are determined (see code for values)
 
-		# Close socket
-		s.close()
+        Parameters:
+            R_PT100 (float):
+                Resistance measured in the PT-100
 
-		self._initialized=True
+        Returns (float):
+            Temperature in Celsius
 
-	def _get_resistance(self):
-		assert self._initialized, "Cannot run uninitialized machine"
+        Example:
+            >>> s = pt100()
+            >>> print(round(s.ohm2celsius(110), 2))
+            25.69
+        """
+        R0 = 100.0
+        c = [-1.138628e-03, 2.559038e+00, 9.988525e-04, -1.061679e-06,
+             2.585121e-08]
 
-		s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		s.connect((self._host,self._tcp_port))
-		s.setblocking(0)	# make socket non blocking
+        temp = c[4]
+        for n in [3, 2, 1, 0]:
+            temp = temp * (R_PT100 - R0) + c[n]
+        return temp
 
-		# Set address
-		s.sendall(b'++addr '+str(self._address).encode('ascii')+'\r\n')
-		
-		# Trigger measurement, ask for results
-		s.sendall(b'T3\r\n')
+    def init(self):
+        """Initializes the machine by trying out the connections.
 
-		# Set instrument to talk
-		s.sendall(b'++read eoi\r\n')
+        The PT-100 cannot be initialized before running this command
+        """
+        assert not self._initialized, "Cannot init initialized machine"
 
-		# Get the answer
-		t0,ready=time.time(),False
-		while time.time()-t0<self._timeout and not ready:
-			try:
-				answ=s.recv(1024)
-				time.sleep(.5)
-				try: answ=answ+s.recv(1024)
-				except: pass
-				ready=True
-			except: pass
-			time.sleep(.1)
+        # Open socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self._host, self._tcp_port))
+        s.setblocking(0)    # make socket non blocking
 
-		# Close socket
-		s.close()
+        # Set address
+        s.sendall(b'++addr '+str(self._address).encode('ascii')+'\r\n')
 
-		if ready: return float(answ.replace('\r\n',''))
-		else: return -1
+        # Send setup
+        s.sendall(b'++clr\r\n')
+        time.sleep(2)
+        s.sendall(b'F3 R2 N5 Z1 T4 M35 \r\n')
+        time.sleep(2)
+        s.sendall(b'F3 R2 N5 Z1 T4 M35 \r\n')
 
-	def get_temperature(self, unit='K'):
-		assert self._initialized, "Cannot run uninitialized machine"
+        # Close socket
+        s.close()
 
-		res=-1
-		while res<0:
-			res=self._get_resistance()
-			if res<0: time.sleep(.5)
+        self._initialized = True
 
-		# Convert resistance to temperature
-		temp=self.ohm2celsius(res)
-		if unit == 'K': return temp+273.15
-		else: return temp
+    def _get_resistance(self):
+        assert self._initialized, "Cannot run uninitialized machine"
 
-	def close(self):
-		assert self._initialized, "Cannot close uninitialized machine"
-		self._initialized=False
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self._host, self._tcp_port))
+        s.setblocking(0)    # make socket non blocking
+
+        # Set address
+        s.sendall(b'++addr '+str(self._address).encode('ascii')+'\r\n')
+
+        # Trigger measurement, ask for results
+        s.sendall(b'T3\r\n')
+
+        # Set instrument to talk
+        s.sendall(b'++read eoi\r\n')
+
+        # Get the answer
+        t0, ready = time.time(), False
+        while time.time() - t0 < self._timeout and not ready:
+            try:
+                answ = s.recv(1024)
+                time.sleep(.5)
+                try:
+                    answ = answ + s.recv(1024)
+                except:
+                    pass
+                ready = True
+            except:
+                pass
+            time.sleep(.1)
+
+        # Close socket
+        s.close()
+
+        if ready:
+            return float(answ.replace('\r\n', ''))
+        else:
+            return -1
+
+    def get_temperature(self, unit='K'):
+        """Returns the temperature using ohm2celsius after reading the PT-100
+
+        The pt100 has to be initialized before running this command
+
+        Parameters:
+            unit (str):
+                 "K" adds 273.15 to returned result, other inputs does not
+
+        Returns (float):
+            Temperature as read from the resistor
+
+        Example:
+            >>> print(pt100.get_temperature())
+            295.7
+            >>> print(pt100.get_temperature('anything'))
+            22.55
+        """
+        assert self._initialized, "Cannot run uninitialized machine"
+
+        res = -1
+        while res < 0:
+            res = self._get_resistance()
+            if res < 0:
+                time.sleep(.5)
+
+        # Convert resistance to temperature
+        temp = self.ohm2celsius(res)
+        if unit == 'K':
+            return temp+273.15
+        else:
+            return temp
+
+    def close(self):
+        """Closes the machine
+
+        Must be initialized already, and will set itself in an uninitialized
+        state
+        """
+        assert self._initialized, "Cannot close uninitialized machine"
+        self._initialized = False
